82131efc505444ed13e04b874578a7dc85bcc03a
George Coll <george.r.coll@gmail.com>
2017-10-27 16:26:24 -0500

    Backend set up and running under K9BackEnd folder

---
+++ K9BackEnd/src/main/webapp/app/app.module.ts
@@ -0,0 +1,55 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { K9BackEndSharedModule, UserRouteAccessService } from './shared';
+import { K9BackEndHomeModule } from './home/home.module';
+import { K9BackEndAdminModule } from './admin/admin.module';
+import { K9BackEndAccountModule } from './account/account.module';
+import { K9BackEndEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        K9BackEndSharedModule,
+        K9BackEndHomeModule,
+        K9BackEndAdminModule,
+        K9BackEndAccountModule,
+        K9BackEndEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class K9BackEndAppModule {}
