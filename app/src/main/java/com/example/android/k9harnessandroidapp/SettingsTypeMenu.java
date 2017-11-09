97496f4ef94152931d6abd7832f1103cc0414e46
rickflaget <rnflaget@crimson.ua.edu>
2017-11-09 13:04:56 -0600

    Updated Settings Page Buttons and Methods.

--- app/src/main/java/com/example/android/k9harnessandroidapp/SettingsTypeMenu.java
+++ app/src/main/java/com/example/android/k9harnessandroidapp/SettingsTypeMenu.java
@@ -1,7 +1,9 @@
 package com.example.android.k9harnessandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsTypeMenu extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class SettingsTypeMenu extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_type_menu);
+    }
+
+    public void goToSettingsDog(View view) {
+        Intent goToSettingsDogIntent = new Intent(this, SettingsDog.class);
+        startActivity(goToSettingsDogIntent);
+    }
+
+    public void goToSettingsAccount(View view) {
+        Intent goToSettingsAccountIntent = new Intent(this, SettingsAccount.class);
+        startActivity(goToSettingsAccountIntent);
+    }
+
+    public void goToSettingsBluetooth(View view) {
+        Intent goToSettingsBluetoothIntent = new Intent(this, SettingsBluetooth.class);
+        startActivity(goToSettingsBluetoothIntent);
+    }
+
+    public void goToSettingsNotification(View view) {
+        Intent goToSettingsNotificationIntent = new Intent(this, SettingsNotifications.class);
+        startActivity(goToSettingsNotificationIntent);
     }
 }
