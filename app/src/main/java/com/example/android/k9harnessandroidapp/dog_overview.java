05399539787e8d2feef4ddf0c26bcc5ade5bfee1
jdhawley <jdhawley@crimson.ua.edu>
2017-11-09 14:40:34 -0600

    Created empty activity for Dog overview

---
+++ app/src/main/java/com/example/android/k9harnessandroidapp/dog_overview.java
@@ -0,0 +1,13 @@
+package com.example.android.k9harnessandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dog_overview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_overview);
+    }
+}
