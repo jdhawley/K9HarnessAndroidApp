8210458a90961b6390af0132d7dc333b50e71e8d
jdhawley <jdhawley@crimson.ua.edu>
2017-11-20 01:22:56 -0600

    Refactored DogOverview.java to make it a little more modular.

--- app/src/main/java/com/example/android/k9harnessandroidapp/DogOverview.java
+++ app/src/main/java/com/example/android/k9harnessandroidapp/DogOverview.java
@@ -1,31 +1,128 @@
 package com.example.android.k9harnessandroidapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.Series;
 
 import java.util.Random;
 
 public class DogOverview extends AppCompatActivity {
-    private boolean TESTING_MESSAGES = false;
-    private int testSeconds = 0;
-    private int testSecondsIncrementer = 1;
+    private int seconds = 0;
+    private int secondsIncrementer = 1;
+
     private LineGraphSeries<DataPoint> hrSeries = new LineGraphSeries<>();
     private LineGraphSeries<DataPoint> rrSeries = new LineGraphSeries<>();
     private LineGraphSeries<DataPoint> ctSeries = new LineGraphSeries<>();
     private LineGraphSeries<DataPoint> abTSeries = new LineGraphSeries<>();
 
+    private int MAX_DATA_POINTS = 30;
+    private GraphView hrGraph;
+    private GraphView rrGraph;
+    private GraphView ctGraph;
+    private GraphView abtGraph;
+
+    // TODO: Read high/low values from settings and set them here
+    // TODO: Change series color based on value in respect to high/low values
+//    private int hrHigh;
+//    private int hrLow;
+//    private int rrHigh;
+//    private int rrLow;
+//    private int ctHigh;
+//    private int ctLow;
+//    private int abtHigh;
+//    private int abtLow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_overview);
+
+        initializeGraphs();
+    }
+
+    private void initializeGraphs(){
+        hrGraph = (GraphView) findViewById(R.id.hrGraph);
+        rrGraph = (GraphView) findViewById(R.id.rrGraph);
+        ctGraph = (GraphView) findViewById(R.id.ctGraph);
+        abtGraph = (GraphView) findViewById(R.id.abtGraph);
+
+
+        hrGraph.getViewport().setXAxisBoundsManual(true);
+        rrGraph.getViewport().setXAxisBoundsManual(true);
+        ctGraph.getViewport().setXAxisBoundsManual(true);
+        abtGraph.getViewport().setXAxisBoundsManual(true);
+
+        updateGraphAxes();
+    }
+
+    private void updateGraphAxes(){
+        if(seconds < 6){
+            hrGraph.getViewport().setMaxX(6);
+            rrGraph.getViewport().setMaxX(6);
+            ctGraph.getViewport().setMaxX(6);
+            abtGraph.getViewport().setMaxX(6);
+        }
+        else {
+            hrGraph.getViewport().setMaxX(seconds);
+            rrGraph.getViewport().setMaxX(seconds);
+            ctGraph.getViewport().setMaxX(seconds);
+            abtGraph.getViewport().setMaxX(seconds);
+        }
+
+        if(seconds - MAX_DATA_POINTS >= 0) {
+            hrGraph.getViewport().setMinX((seconds - MAX_DATA_POINTS) + 1);
+            rrGraph.getViewport().setMinX((seconds - MAX_DATA_POINTS) + 1);
+            ctGraph.getViewport().setMinX((seconds - MAX_DATA_POINTS) + 1);
+            abtGraph.getViewport().setMinX((seconds - MAX_DATA_POINTS) + 1);
+        }
+        else{
+            hrGraph.getViewport().setMinX(0);
+            rrGraph.getViewport().setMinX(0);
+            ctGraph.getViewport().setMinX(0);
+            abtGraph.getViewport().setMinX(0);
+        }
+    }
+
+    private int[] processDataString(String dataString){
+        // Clean the "#" off the end of the string and split the values into an array
+        String[] stringData = dataString.replace("#", "").split(":");
+
+        int[] intData = new int[5];
+        for(int i = 0; i < 5; i++){
+            intData[i] = Integer.parseInt(stringData[i]);
+        }
+
+        return intData;
+    }
+
+    public void addDataPoint(View view){
+        // TODO: Implement functionality to read actual data instead of random generation.
+        String dp = generateDataPoint();
+        int[] data = processDataString(dp);
+
+        int hr = data[0];
+        int rr = data[1];
+        int ct = data[2];
+        //int amt = data[3];  // this value is ignored because it is not used at this time
+        int abt = data[4];
+
+        hrSeries.appendData(new DataPoint(seconds, hr), true, MAX_DATA_POINTS);
+        rrSeries.appendData(new DataPoint(seconds, rr), true, MAX_DATA_POINTS);
+        ctSeries.appendData(new DataPoint(seconds, ct), true, MAX_DATA_POINTS);
+        abTSeries.appendData(new DataPoint(seconds, abt), true, MAX_DATA_POINTS);
+
+        hrGraph.addSeries(hrSeries);
+        rrGraph.addSeries(rrSeries);
+        ctGraph.addSeries(ctSeries);
+        abtGraph.addSeries(abTSeries);
+
+        updateGraphAxes();
+
+        seconds += secondsIncrementer;
     }
 
     private String generateDataPoint(){
@@ -33,76 +130,8 @@ public class DogOverview extends AppCompatActivity {
         int hr = r.nextInt(41) + 60;
         int rr = r.nextInt(26) + 10;
         int ct = r.nextInt(3) + 101;
-        int amT = r.nextInt(3) + 101;
-        int abT = r.nextInt(3) + 101;
-        return hr + ":" + rr + ":" + ct + ":" + amT + ":" + abT + "#";
-    }
-
-    public void addDataPoint(View view){
-        int MAX_DATA_POINTS = 30;
-
-        String dp = generateDataPoint();
-        // Clean the "#" off the end of the data string.
-        dp = dp.replace("#", "");
-        String data[] = dp.split(":");
-
-        int hr = Integer.parseInt(data[0]);
-        int rr = Integer.parseInt(data[1]);
-        int ct = Integer.parseInt(data[2]);
-        int amT = Integer.parseInt(data[3]);
-        int abT = Integer.parseInt(data[4]);
-
-        if(TESTING_MESSAGES) {
-            String message = hr + ":" + rr + ":" + ct + ":" + abT + "#" + testSeconds;
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-        }
-
-        hrSeries.appendData(new DataPoint(testSeconds, hr), true, MAX_DATA_POINTS);
-        rrSeries.appendData(new DataPoint(testSeconds, rr), true, MAX_DATA_POINTS);
-        ctSeries.appendData(new DataPoint(testSeconds, ct), true, MAX_DATA_POINTS);
-        abTSeries.appendData(new DataPoint(testSeconds, abT), true, MAX_DATA_POINTS);
-
-        GraphView hrGraph = (GraphView) findViewById(R.id.hrGraph);
-        GraphView rrGraph = (GraphView) findViewById(R.id.rrGraph);
-        GraphView ctGraph = (GraphView) findViewById(R.id.ctGraph);
-        GraphView abTGraph = (GraphView) findViewById(R.id.abTGraph);
-
-        hrGraph.addSeries(hrSeries);
-        rrGraph.addSeries(rrSeries);
-        ctGraph.addSeries(ctSeries);
-        abTGraph.addSeries(abTSeries);
-
-        hrGraph.getViewport().setXAxisBoundsManual(true);
-        rrGraph.getViewport().setXAxisBoundsManual(true);
-        ctGraph.getViewport().setXAxisBoundsManual(true);
-        abTGraph.getViewport().setXAxisBoundsManual(true);
-
-        if(testSeconds < 6){
-            hrGraph.getViewport().setMaxX(6);
-            rrGraph.getViewport().setMaxX(6);
-            ctGraph.getViewport().setMaxX(6);
-            abTGraph.getViewport().setMaxX(6);
-        }
-        else {
-            hrGraph.getViewport().setMaxX(testSeconds);
-            rrGraph.getViewport().setMaxX(testSeconds);
-            ctGraph.getViewport().setMaxX(testSeconds);
-            abTGraph.getViewport().setMaxX(testSeconds);
-        }
-
-        if(testSeconds - MAX_DATA_POINTS >= 0) {
-            hrGraph.getViewport().setMinX((testSeconds - MAX_DATA_POINTS) + 1);
-            rrGraph.getViewport().setMinX((testSeconds - MAX_DATA_POINTS) + 1);
-            ctGraph.getViewport().setMinX((testSeconds - MAX_DATA_POINTS) + 1);
-            abTGraph.getViewport().setMinX((testSeconds - MAX_DATA_POINTS) + 1);
-        }
-        else{
-            hrGraph.getViewport().setMinX(0);
-            rrGraph.getViewport().setMinX(0);
-            ctGraph.getViewport().setMinX(0);
-            abTGraph.getViewport().setMinX(0);
-        }
-
-        testSeconds += testSecondsIncrementer;
+        int amt = r.nextInt(3) + 101;
+        int abt = r.nextInt(3) + 101;
+        return hr + ":" + rr + ":" + ct + ":" + amt + ":" + abt + "#";
     }
 }
