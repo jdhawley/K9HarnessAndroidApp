8d39c978c3cba88acf4e2f717346b87504d34746
rickflaget <rnflaget@crimson.ua.edu>
2017-11-22 00:02:47 -0600

    Added start of Compound Switch Listener to SettingsNotification.java

--- app/src/main/java/com/example/android/k9harnessandroidapp/SettingsNotifications.java
+++ app/src/main/java/com/example/android/k9harnessandroidapp/SettingsNotifications.java
@@ -2,6 +2,7 @@ package com.example.android.k9harnessandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 public class SettingsNotifications extends AppCompatActivity {
 
@@ -9,5 +10,31 @@ public class SettingsNotifications extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_notifications);
+
+        CompoundButton.OnCheckedChangeListener multiListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switch(compoundButton.getId()){
+                    case R.id.switch_hr_low:
+                        break;
+                    case R.id.switch_hr_high:
+                        break;
+                    case R.id.switch_rr_low:
+                        break;
+                    case R.id.switch_rr_high:
+                        break;
+                    case R.id.switch_ct_low:
+                        break;
+                    case R.id.switch_ct_high:
+                        break;
+                    case R.id.switch_at_low:
+                        break;
+                    case R.id.switch_at_high:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 }
