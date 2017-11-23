b8e93e98df5f7cbd22db616b0a6bf7e7b2c7de4a
caribbeansteve <george.r.coll@gmail.com>
2017-11-23 10:28:24 -0600

    Added shared preferences for loading data between sessions, and begun working on the RestService for requesting database data

---
+++ app/src/main/java/com/example/android/k9harnessandroidapp/RestExampleActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.android.k9harnessandroidapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RestExampleActivityFragment extends Fragment {
+
+    public RestExampleActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rest_example, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/android/k9harnessandroidapp/UserPreferences.java
@@ -0,0 +1,24 @@
+package com.example.android.k9harnessandroidapp;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by George on 11/23/2017.
+ */
+
+public class UserPreferences {
+    SharedPreferences pref;
+
+    public UserPreferences(Activity activity) {
+        pref = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    String getDog() {
+        return pref.getString("dog", "No Dog Found");
+    }
+
+    void setDog(String dog) {
+        pref.edit().putString("dog", dog).commit();
+    }
+}
