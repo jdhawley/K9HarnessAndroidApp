b2ddfcfea65c25a389f60e6ea9e0d6eb1c309430
rickflaget <rnflaget@crimson.ua.edu>
2017-12-13 03:22:16 -0600

    Nav added to Dog Measurement Pages

--- app/src/main/java/com/example/android/k9harnessandroidapp/DogMeasurement.java
+++ app/src/main/java/com/example/android/k9harnessandroidapp/DogMeasurement.java
@@ -2,14 +2,22 @@ package com.example.android.k9harnessandroidapp;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-public class DogMeasurement extends AppCompatActivity {
+public class DogMeasurement extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private SQLiteHelper db;
     private int MAX_DATA_POINTS = 30;
 
@@ -19,6 +27,7 @@ public class DogMeasurement extends AppCompatActivity {
     private LineGraphSeries<DataPoint> historicalLowSeries = new LineGraphSeries<>();
 
     private PageType pageType = null;
+
 
     private enum PageType {
         HeartRate,
@@ -47,6 +56,18 @@ public class DogMeasurement extends AppCompatActivity {
         historicalGraph = findViewById(R.id.historicalGraph);
 
         setTitle();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dog_measurement);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 //        setActivityDefaults();
 
         new Thread(new Runnable() {
@@ -148,4 +169,97 @@ public class DogMeasurement extends AppCompatActivity {
             Log.e("MeasurementActivity", "PageType not set properly.");
         }
     }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.account_settings);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Navigation nav = new Navigation();
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.nav_dog) {
+            nav.goToSettingsDog(this);
+            finish();
+            return true;
+            // Handle the camera action
+        } else if (id == R.id.nav_account) {
+            nav.goToSettingsAccount(this);
+            finish();
+            //THIS IS THIS ACTIVITY
+            return true;
+
+        } else if (id == R.id.nav_bluetooth) {
+            nav.goToSettingsBluetooth(this);
+            finish();
+            return true;
+
+        } else if (id == R.id.nav_notification) {
+            nav.goToSettingsNotification(this);
+            finish();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // Handle navigation view item clicks here.
+        Navigation nav = new Navigation();
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dog_overview){
+            nav.goToDogOverview(this);
+            finish();
+        }
+        else if (id == R.id.nav_heart_rate && pageType != PageType.HeartRate){
+            nav.goToHeartRateActivity(this);
+            finish();
+        }
+        else if (id == R.id.nav_resp_rate && pageType != PageType.RespiratoryRate){
+            nav.goToRespiratoryRateActivity(this);
+            finish();
+        }
+        else if (id == R.id.nav_core_temp && pageType != PageType.CoreTemperature){
+            nav.goToCoreTemperatureActivity(this);
+            finish();
+        }
+        else if (id == R.id.nav_ab_temp && pageType != PageType.AbdominalTemperature){
+            nav.goToAbdominalTemperatureActivity(this);
+            finish();
+        }
+        else if (id == R.id.nav_logOut) {
+            //TODO: logout function!
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dog_measurement);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
